2de2ed1947658860fcf6462758fc34d3b0b83e17
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-11-07 11:48:10 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _2DRPGMAP2
-{
+{   
     internal class Program
     {
         static int scale;
-        static int x, y, origx, origy;
+        static int origx, origy;
         static string p;
         static int rows, columns;
+        static ConsoleKeyInfo key;
+        static bool gameOver;
+        static int PlayerPosx;
+        static int PlayerPosy;
 
         static char[,] map = new char[,] // dimensions defined by following data:
     {
@@ -31,49 +35,53 @@ namespace _2DRPGMAP2
         static void Main(string[] args)
         {
             scale = 3;
-            x = 0;
-            y = 0;
             origx = Console.CursorLeft;
             origy = Console.CursorTop;
+            PlayerPosy = origy + 6 * scale;
+            PlayerPosx = origx + 10 * scale;
             p = "P";
+            gameOver = false;
             rows = map.GetLength(0);
             columns = map.GetLength(1);
 
-            Console.WriteLine("The array is " + rows + " rows tall and " + columns + " columns wide.");
-            Console.WriteLine();
+            //Console.WriteLine("The array is " + rows + " rows tall and " + columns + " columns wide.");
+            //Console.WriteLine();
+            //DisplayMap();
+            //Console.WriteLine();
 
-            DisplayMap();
-
-            Console.WriteLine();
-
-            DisplayMap(scale);
-
-            Console.ReadKey();
-        }
-        static void DisplayMap()
-        {
-            Console.BackgroundColor = ConsoleColor.Black;
-
-            for (int x = 0; x < rows; x++)
+            while (gameOver == false)
             {
-                for (int y = 0; y < columns; y++)
-                {
-                    ColourCode(x, y);
-                    Console.Write(map[x, y]); //writes the single character located at array index x, y
-                }    
-                Console.WriteLine(); //line breaks when the current row is done being written
+                DisplayMap(scale);
+                PlayerChoice();
+                PlayerDraw(p, PlayerPosx, PlayerPosy);
+                Console.ReadKey();
             }
-
-            Console.ResetColor();
         }
+        //static void DisplayMap()
+        //{
+        //    Console.BackgroundColor = ConsoleColor.Black;
+            
+        //    for (int x = 0; x < rows; x++)
+        //    {
+        //        for (int y = 0; y < columns; y++)
+        //        {
+        //            ColourCode(x, y);
+        //            Console.Write(map[x, y]); //writes the single character located at array index x, y
+        //        }    
+        //        Console.WriteLine(); //line breaks when the current row is done being written
+        //    }
+
+        //    Console.ResetColor();
+        //}
         static void DisplayMap(int scale)
         {
             int bordersize = columns * scale;            
             Console.BackgroundColor = ConsoleColor.Black;
-            for (int r = 0; r < bordersize; r++)
+
+            for (int r = -2; r < bordersize; r++)
             {
                 Console.Write("#");
-            }
+            }            
             Console.WriteLine();
             for (int x = 0; x < rows; x++)
             {
@@ -91,7 +99,7 @@ namespace _2DRPGMAP2
                 }
             }
             Console.BackgroundColor = ConsoleColor.Black;
-            for (int r = 0; r < bordersize; r++)
+            for (int r = -2; r < bordersize; r++)
             {
                 Console.Write("#");
             }
@@ -120,7 +128,38 @@ namespace _2DRPGMAP2
         }
         static void PlayerChoice()
         {
+            Console.WriteLine();
+            Console.WriteLine("Press 'W' to move North, 'A' to move West, 'S' to move South, or 'D' to move East. Press 'ESC' to Quit.");
 
+            key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                gameOver = true;
+            }
+            else if (key.Key == ConsoleKey.W)
+            {
+                PlayerPosy--;
+            }
+            else if (key.Key == ConsoleKey.A)
+            {
+                PlayerPosx--;
+            }
+            else if (key.Key == ConsoleKey.S)
+            {
+                PlayerPosy++;
+            }
+            else if (key.Key == ConsoleKey.D)
+            {
+                PlayerPosx++;
+            }
+
+        }
+        static void PlayerDraw(string p, int PlayerPosx, int PlayerPosy)
+        {
+            int OldPlayerPosx = PlayerPosx;
+            int OldPlayerPosy = PlayerPosy;
+            Console.SetCursorPosition(OldPlayerPosx + PlayerPosx, OldPlayerPosy + PlayerPosy);
+            Console.Write(p);
         }
     }
 }
